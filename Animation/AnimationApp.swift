9d9ca11118ec243c401a8c562d009523cda0a20a
jake <palatable7@naver.com>
2022-10-02 20:28:15 +0900

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by 김종권 on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
